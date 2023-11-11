ef7c1e730fbd3b5546551226d9cf9d3e8f67c42d
Your Name <you@example.com>
2023-11-11 20:34:45 +0100

    Created join and leave buttons, added a list of prizes and names

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -3,55 +3,49 @@
 namespace App\Controller;
 
 use App\Entity\Event;
-use App\Form\RegEventFormType;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class EventsController extends AbstractController
 {
     #[Route('/events/{id}', name: 'event_show')]
-    public function show(EntityManagerInterface $EntityManager, int $id, Request $request): Response
+    public function show( Event $event): Response
+    {   
+        $namearray = $event->getUsers();
+        
+        return $this->render('events/show.html.twig', [
+            'event' => $event,
+            'namearray' => $namearray
+        ]);
+    }
+
+    #[Route('/events/join/{id}', name: 'event_join')]
+    public function joinEvent(EntityManagerInterface $entityManager, Event $event): RedirectResponse
     {
-        /** @var \App\Entity\Event $event */
-        $event = $EntityManager->getRepository(Event::class)->find($id);
-        
-        if (!$event) {
-            throw $this->createNotFoundException(
-                'No event found for id '.$id
-            );
-        }
+        $event->addUser($this->GetUser());
+        $entityManager->flush($event);
 
-        $form = $this->createform(RegEventFormType::class);
-        $form->handleRequest($request);
-        if($form->isSubmitted()){
-            if($form->getData()['isAttending']){
-                $event->addUser($this->GetUser());
-                $EntityManager->flush($event); 
-            }
-            else{
-                $event->removeUser($this->getUser());
-                $EntityManager->flush($event);
-            }
-        }
+        return $this->redirectToRoute('event_show', ['id' => $event->getId()]);
+    }
 
-        $namearray=[];
-        $userarray=$event->getUsers();
-        foreach($userarray as $user){
-            array_push($namearray, $user->getUsername());
-        }
-        
+    #[Route('/events/leave/{id}', name: 'event_leave')]
+    public function leaveEvent(EntityManagerInterface $entityManager, Event $event): RedirectResponse
+    {
+        $event->removeUser($this->GetUser());
+        $entityManager->flush($event);
 
-        return $this->render('events/show.html.twig', ['event' => $event, 'form' => $form, 'namearray' => $namearray]);
+        return $this->redirectToRoute('event_show', ['id' => $event->getId()]);
     }
 
     #[Route('/events', name: 'app_events')]
     public function index(EventRepository $eventRepository): Response
     {
         $events = $eventRepository->findAll();
+
         return $this->render('events/events.html.twig', ['events' => $events]);
     }
 }
